8f589fa5c5578740de7be782c821ba35b3dba48c
Nahuel Lavrut <nahuellavrut88@gmail.com>
2020-11-30 17:55:57 -0300

    Change env

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://nahuelgranate:czVlb7n4b62x9GGm@cluster0.zcgq3.mongodb.net/cafe'
+    urlDB = process.env.DB_URI;
+    // urlDB = 'mongodb+srv://nahuelgranate:czVlb7n4b62x9GGm@cluster0.zcgq3.mongodb.net/cafe'
 }
 
 process.env.DATABASE = urlDB;
